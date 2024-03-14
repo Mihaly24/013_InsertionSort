604f3b3d703b907abcaf35624bf4350d45186ab5
Mihaly24 <fairuzk20@gmail.com>
2024-03-14 18:21:26 +0700

    Menambahkan insertionsort

--- InsertSort/InsertSort.cpp
+++ InsertSort/InsertSort.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 using namespace std;
 
+int arr[9];
+int n;
+
+
+
 void insertionsort() {
-    int i, n, temp, j;
-    int arr[9];
+    int i, temp, j;
+	
+    for (i = 1; i <= n - 1; i++) { //step1
 
+        temp = arr[i]; //step 2
 
+        j = i - 1; //step 3
+
+        while (j >= 0 && arr[j] > temp) //step 4
+        {
+            arr[j + 1] = arr[j]; //step 4a
+            j--; //step 4b
+        }
+
+        arr[j + 1] = temp; //step 5
+    }
 }
 
-int main()
-{
-    
-}
+
+
